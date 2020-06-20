0352f6a15acdee927bd05510a72cc73cd3896be5
Eithel.Vega <eithel.vega@ucr.ac.cr>
2020-06-20 18:30:28 -0400

    parte 1

---
+++ proyect.py
@@ -0,0 +1,6 @@
+print("Bienvenido al juego de Yahtzee")
+
+eleccion = 0
+while eleccion<5:
+      print("\n1 Jugar\n2 Mostrar estadisticas\n3 Ayuda\n4 Acerca de...\n5 Salir\n")
+      eleccion = int(input("Para seleccionar una opcion inserte el numero correspondiente: "))
