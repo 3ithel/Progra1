ee371099c85838dc8e0fe3643186419a2ff70523
Eithel.Vega <eithel.vega@ucr.ac.cr>
2020-06-21 00:30:20 -0400

    se agrega el acerca de, faltan detalles menores

--- proyect.py
+++ proyect.py
@@ -1,6 +1,13 @@
+#Aqui arriba iran las funciones
+#funcion para "Acerca de", jaj creo que es una obviedad
+acercaDe = "\n---Acerca de---\nNombre del programa: Yahtzee modificado\n" \
+           "Version: aun por definir (recordar las tildes)\n" \
+           "Programadores: Edwin Urbina y Eithel Vega\n"
+#aqui se ejecuta el menu
 print("Bienvenido al juego de Yahtzee")
-
-eleccion = 0
-while eleccion<5:
+eleccion = 1
+while eleccion<5 and eleccion>0:
       print("\n1 Jugar\n2 Mostrar estadisticas\n3 Ayuda\n4 Acerca de...\n5 Salir\n")
       eleccion = int(input("Para seleccionar una opcion inserte el numero correspondiente: "))
+      if eleccion == 4:
+            print(acercaDe)
